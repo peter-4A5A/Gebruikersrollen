54637a2303b1f4ae5858d844309dddec39fb849f
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-02 11:26:45 +0200

    Added header

---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="row1">
+  <header id="header" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.html">Cooban</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li><a href="index.php">Home</a></li>
+        <li><a class="drop" href="#">Voorraden</a>
+          <ul>
+            <li><a href="voorraad.php">Voorraden</a></li>
+            <li><a href="producten.php">Producten</a></li>
+          </ul>
+        </li>
+        <li><a href="locaties.php">Locaties</a></li>
+        <li class="active"><a href="gebruikers.php">Gebruikers</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
