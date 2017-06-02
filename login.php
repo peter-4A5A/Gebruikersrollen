6958f071b70d3993e52c0a6b5cf3eb4571a1195a
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-02 12:00:31 +0200

    Styled login form

--- login.php
+++ login.php
@@ -3,20 +3,99 @@
   require_once 'classes/user.class.php';
   $User = new User();
 
-  if (ISSET($_REQUEST['user'])) {
-    $User->userLogin($_REQUEST['user_mail'], $_REQUEST['user_password'], "/leerjaar2/php/Gebruikersrollen/");
-
-    echo "<pre>";
-      var_dump($_SESSION);
-    echo "</pre>";
+  if (ISSET($_POST['user'])) {
+    echo $User->userLogin($_REQUEST['user_mail'], $_REQUEST['user_password'], "/leerjaar2/php/Gebruikersrollen/");
+  }
+  else if (ISSET($_GET['logout'])) {
+    if ($_GET['logout'] == true) {
+      $User->userLogout('/leerjaar2/php/Gebruikersrollen/');
+    }
   }
 
 ?>
-<form method="post">
-  <div>Email</div>
-  <input type="mail" name="user_mail">
-  <div>Password</div>
-  <input type="password" name="user_password">
-  <div></div>
-  <input type="submit" name="user" value="login">
-</form>
+<!DOCTYPE html>
+<!--
+Template Name: Cooban
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html>
+<head>
+<title>Cooban | Pages | Full Width</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');">
+  <!-- ################################################################################################ -->
+
+  <?php include 'header.php'; ?>
+
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="breadcrumb" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear">
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="content">
+      <!-- ################################################################################################ -->
+      <form method="post">
+        <div>Email</div>
+        <input type="mail" name="user_mail">
+        <div>Password</div>
+        <input type="password" name="user_password">
+        <div></div>
+        <input type="submit" name="user" value="login">
+      </form>
+      <!-- ################################################################################################ -->
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear">
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<!-- IE9 Placeholder Support -->
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!-- / IE9 Placeholder Support -->
+</body>
+</html>
