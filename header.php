a6567b450bf9ba6e0d04aeb1e038024a8dbf98e6
Peter 4A5A <498883@edu.rocmn.nl>
2017-06-04 13:43:24 +0200

    Changed header

--- header.php
+++ header.php
@@ -1,25 +1,55 @@
-<div class="row1">
-  <header id="header" class="hoc clear">
-    <!-- ################################################################################################ -->
-    <?php if (ISSET($_SESSION['userMail'])){echo "Gebruiker: " . $_SESSION['userMail'];} ?>
-    <div id="logo" class="fl_left">
-      <h1><a href="index.html">Cooban</a></h1>
-    </div>
-    <nav id="mainav" class="fl_right">
-      <ul class="clear">
-        <li><a href="index.php">Home</a></li>
-        <li><a class="drop" href="#">Voorraden</a>
-          <ul>
-            <li><a href="voorraad.php">Voorraden</a></li>
-            <li><a href="producten.php">Producten</a></li>
+<?php
+
+  $User = new User();
+  $User->setPageAcces(['admin']);
+  if ($User->clientIfUserHasAcces()) {
+    echo '<div class="row1">
+      <header id="header" class="hoc clear">
+        <!-- ################################################################################################ -->
+        <div id="logo" class="fl_left">
+          <h1><a href="index.html">Cooban</a></h1>
+        </div>
+        <nav id="mainav" class="fl_right">
+          <ul class="clear">
+            <li><a href="index.php">Home</a></li>
+            <li><a class="drop" href="#">Voorraden</a>
+              <ul>
+                <li><a href="voorraad.php">Voorraden</a></li>
+                <li><a href="producten.php">Producten</a></li>
+              </ul>
+            </li>
+            <li><a href="locaties.php">Locaties</a></li>
+            <li><a href="gebruikers.php">Gebruikers</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="login.php?logout=true">Logout</a></li>
           </ul>
-        </li>
-        <li><a href="locaties.php">Locaties</a></li>
-        <li><a href="gebruikers.php">Gebruikers</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="login.php?logout=true">Logout</a></li>
-      </ul>
-    </nav>
-    <!-- ################################################################################################ -->
-  </header>
-</div>
+        </nav>
+        <!-- ################################################################################################ -->
+      </header>
+    </div>' ;
+  }
+  $User->setPageAcces(['voorraad']);
+  else if ($User->clientIfUserHasAcces()) {
+
+  }
+  else {
+    echo '
+    <div class="row1">
+      <header id="header" class="hoc clear">
+        <!-- ################################################################################################ -->
+        <div id="logo" class="fl_left">
+          <h1><a href="index.html">Cooban</a></h1>
+        </div>
+        <nav id="mainav" class="fl_right">
+          <ul class="clear">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="login.php?logout=true">Logout</a></li>
+          </ul>
+        </nav>
+        <!-- ################################################################################################ -->
+      </header>
+    </div>
+    ';
+  }
+?>
